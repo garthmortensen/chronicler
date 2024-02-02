5673602c1eb73264851120e803f21f24054ded01
Garth Mortensen <mortensengarth@hotmail.com>
2024-02-02 08:54:34 -0600

    get calling script works but not logging

--- my_script.py
+++ my_script.py
@@ -1,7 +1,12 @@
 from practice import hello_world
 from practice import print_file_name_of_calling_script
 from practice import print_directory_of_calling_script
+import logging
 
-hello_world()
+
 print_file_name_of_calling_script()
 print_directory_of_calling_script()
+
+def hello_world():
+    logging.info("Hello, world!")
+    print("its nice")
--- practice.py
+++ practice.py
@@ -2,9 +2,46 @@ import logging
 import os
 import inspect
 
+
+def config_logs():
+
+    # define logging directory
+    home = os.path.expanduser("~")
+    log_dir = os.path.join(home, "chronicler_logs")
+
+    # dont overwrite existing log files
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # get calling script
+    calling_script = inspect.stack()[1]
+
+    # define log file name
+    file_name = os.path.basename(calling_script.filename)
+    filename_wo_extension = os.path.splitext(file_name)[0]
+    log_file = os.path.join(log_dir, f"{filename_wo_extension}.log")
+
+    log_format = "%(asctime)s %(filename)s:%(lineno)d %(funcName)s| %(message)s"
+
+    handlers=[  # write stdout and errout
+        logging.FileHandler(filename=log_file, mode="w"),  # writes to file
+        logging.StreamHandler()  # streams to console
+    ]
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=log_format,
+        handlers=handlers
+    )
+
 def hello_world():
+    config_logs()
     logging.info("Hello, world!")
     print("its nice")
+
+def hello_world_logger():
+    """function can be imported into another script and when called, will log 'Hello, world!'"""
+    logging.info("Hello, world!")
 
 def print_file_name_of_calling_script():
     """function can be imported into another script and when called, will print the filename of the calling script"""
