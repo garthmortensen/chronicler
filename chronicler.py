afd4b547b62fe61691b59aa6556a109663dfbd67
Garth Mortensen <mortensengarth@hotmail.com>
2024-02-02 16:35:27 -0600

    Add typehints, docstrings

--- chronicler.py
+++ chronicler.py
@@ -1,133 +1,100 @@
+import os
 import logging
-import subprocess
-import time
-import platform  # get OS and Python version
-import getpass  # get username
-import os # get dir
-import datetime  # format datetime
-import yaml  # read yaml log config
-import inspect # get calling script
+import platform
+import getpass
+import time  # timestamp, timers and sleep
+import subprocess  # for git info
 
-# TODO: this is dumb. refactor somehow
-# TODO: make this importable
-# TODO: OOP?
-date_format = "%Y%m%d_%H%M%S"  # compact ISO 8601, less T
-start_time = time.time()
 
-def define_start_time(date_format):
-    start_time = time.time()
-    start_time_str = datetime.datetime.fromtimestamp(start_time).strftime(date_format)
-    return start_time_str
+def log_conf(invoking_filename: str) -> str:
+    """Configure logging for the calling script.
+    Sets up logs to write to a file in the user's home directory and print to console.    
+    Return the log file path.
+    """
 
-def config_logs(date_format):
-    start_time_str = define_start_time(date_format)
-
-    # define logging directory
-    home = os.path.expanduser("~")
-    log_dir = os.path.join(home, "chronicler_logs")
-
-    # dont overwrite existing log files
+    # logging in home dir seems like secure programming
+    log_dir = os.path.join(os.path.expanduser("~"), "chronicles")
     if not os.path.exists(log_dir):
+        print(f"mkdir for logs: {log_dir}")
         os.makedirs(log_dir)
 
-    # define log file name
-    filename = os.path.basename(__file__)
-    filename_wo_extension = os.path.splitext(filename)[0]
-    log_file = os.path.join(log_dir, f"{start_time_str}_{filename_wo_extension}.log")
-
-    log_format = "%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(funcName)s| %(message)s"
+    # this module is meant to be imported and log info about the calling script
+    # use calling script name for log filename; include timestamp for uniqueness.
+    # But if script is run multiple times per second, only the last log remains.
+    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    filename_wo_extension = os.path.splitext(invoking_filename)[0]  # remove .py
+    log_file = os.path.join(log_dir, f"{timestamp}_{filename_wo_extension}.log")
 
     handlers=[  # write stdout and errout
-        logging.FileHandler(filename=log_file, mode="w"),  # writes to file
-        logging.StreamHandler()  # streams to console
+        logging.StreamHandler(),  # stream to console
+        logging.FileHandler(filename=log_file, mode="w")  # write to file
     ]
 
+    # 20240202_154449 INFO practice.py:56 log_meta| pid:    10629
+    log_format = "%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(funcName)s| %(message)s"
     logging.basicConfig(
         level=logging.INFO,
         format=log_format,
-        datefmt=date_format,
+        datefmt="%Y%m%d_%H%M%S",
         handlers=handlers
     )
 
     return log_file
 
 
-def parameterize_logs(log_config_filepath):
-    with open(log_config_filepath, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+def log_meta(invoking_file_dir: str, invoking_filename: str, log_file: str):
+    """Log meta information about the calling script, environmental variables, and git info."""
 
-
-def log_header_info(log_config, log_file, date_format):
-    start_time_str = define_start_time(date_format)
+    # meta info to log
     os_version = platform.platform()
     python_version = platform.python_version()
     whoami = getpass.getuser()
-
-    file_name = os.path.basename(__file__)
-    file_dir = os.path.dirname(os.path.abspath(file_name))
     pid = os.getpid()
 
     # ascii art dividers: determine if os or file string is longer, and remove padding for "key"
-    half_separator = int(max(len(os_version), len(file_name)) / 2) - len("OS: ")
-    top_separator = "✧" + "-" * half_separator + " meta info " + "-" * half_separator + "✧"  # ╔═══ META INFO ═══╗
-    bottom_separator = "✧" + "-" * (len(top_separator) - 2) + "✧"  # ╚════════╝
+    half_length = 25
+    separator_head = "✧" + "-" * half_length + " meta log { " + "-" * half_length + "✧"  # ╔═══ meta log { ═══╗
+    separator_foot = "✧" + "-" * half_length + " meta log } " + "-" * half_length + "✧"  # ╔═══ meta log } ═══╗
 
-    logging.info(top_separator)
-    if log_config.get('log_file'):
-        logging.info(f"log file: {log_file}")
-    if log_config.get('start_time'):
-        logging.info(f"start:    {start_time_str}")
-    if log_config.get('os_version'):
-        logging.info(f"os:       {os_version}")
-    if log_config.get('python_version'):
-        logging.info(f"py ver:   {python_version}")
-    if log_config.get('whoami'):
-        logging.info(f"whoami:   {whoami}")
-    if log_config.get('file_name'):
-        logging.info(f"file:     {file_name}")
-    if log_config.get('file_dir'):
-        logging.info(f"dir:      {file_dir}")
-    if log_config.get('pid'):
-        logging.info(f"pid:      {pid}")
+    logging.info(separator_head)
+    logging.info(f"log:    {log_file}")
+    logging.info(f"dir:    {invoking_file_dir}")
+    logging.info(f"file:   {invoking_filename}")
+    logging.info(f"whoami: {whoami}")
+    logging.info(f"python: {python_version}")
+    logging.info(f"os:     {os_version}")
+    logging.info(f"pid:    {pid}")
 
     # get available git info
-    if log_config.get('git_info'):
-        try:  # if no .git, no branch
-            git_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode("utf-8").strip()
-            logging.info(f"branch:   {git_branch}")
-        except subprocess.CalledProcessError:
-            logging.warning("git branch unavailable")
+    try:  # repo may only be local
+        git_origin_url = subprocess.check_output(["git", "config", "--get", "remote.origin.url"]).decode("utf-8").strip()
+        logging.info(f"url:    {git_origin_url}")
+    except subprocess.CalledProcessError:
+        logging.warning("git url unavailable")
 
-        try:  # after git init, no commits yet
-            git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
-            logging.info(f"hash:     {git_hash[:7]}")  # 8 chars per https://stackoverflow.com/a/18134919
-        except subprocess.CalledProcessError:
-            logging.warning("git hash unavailable")
+    try:  # if no .git, no branch
+        git_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode("utf-8").strip()
+        logging.info(f"branch: {git_branch}")
+    except subprocess.CalledProcessError:
+        logging.warning("git branch unavailable")
 
-        try:  # repo may only be local
-            git_origin_url = subprocess.check_output(["git", "config", "--get", "remote.origin.url"]).decode("utf-8").strip()
-            logging.info(f"url:      {git_origin_url}")
-        except subprocess.CalledProcessError:
-            logging.warning("git url unavailable")
+    try:  # after git init, no commits yet
+        git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
+        logging.info(f"hash:   {git_hash[:7]}")  # 8 chars per https://stackoverflow.com/a/18134919
+    except subprocess.CalledProcessError:
+        logging.warning("git hash unavailable")
 
-    logging.info(bottom_separator)
+    try:  # get commit message header
+        git_commit_message = subprocess.check_output(["git", "log", "-1", "--pretty=%s"]).decode("utf-8").strip()
+        logging.info(f"commit: {git_commit_message}")
+    except subprocess.CalledProcessError:
+        logging.warning("git commit message unavailable")
 
-
-# do something like this
-def setup_logging(script_name=None):
-    log_file = config_logs(date_format)
-    log_config = parameterize_logs("log_config.yaml")
-    log_header_info(log_config, log_file, date_format)
+    logging.info(separator_foot)
 
 
-def testing():
-    # Get the filename and directory of the calling script
-    calling_script = inspect.stack()[1]
-    file_name = os.path.basename(calling_script.filename)
-    file_dir = os.path.dirname(os.path.abspath(calling_script.filename))
-    return file_name, file_dir
-
-
-if __name__ == "__main__":
-    setup_logging()
+def log_init(invoking_file_dir: str, invoking_filename: str):  # all 8 letters for aligned console output
+    """Initialize logging for the calling script.
+    Takes as input the directory and filename of the calling script."""
+    log_file = log_conf(invoking_filename)
+    log_meta(invoking_file_dir, invoking_filename, log_file)
--- my_script.py
+++ my_script.py
@@ -1,19 +1,17 @@
-from practice import log_init
 import logging
+from practice import log_init
 from pathlib import Path
 
 
 def get_script_path_and_file():
-    this_dir = Path(__file__).parent
-    this_file = Path(__file__).name
+    this_dir = str(Path(__file__).parent)
+    this_file = str(Path(__file__).name)
     return this_dir, this_file
-
-this_dir, this_file = get_script_path_and_file()
-
-log_init(this_dir, this_file)
-
 
 def hello_world():
     logging.info("Hello, world!")
+
+this_dir, this_file = get_script_path_and_file()
+log_init(this_dir, this_file)
 
 hello_world()
